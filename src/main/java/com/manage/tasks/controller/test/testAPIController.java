72ad3ac5b585ea369b559360830a5a075f64bb20
Vitor Vilela <vitorvilella123@gmail.com>
2024-02-15 16:00:29 -0300

    Testes added

--- src/main/java/com/manage/tasks/controller/test/testAPIController.java
+++ src/main/java/com/manage/tasks/controller/test/testAPIController.java
@@ -2,6 +2,7 @@ package com.manage.tasks.controller.test;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,9 @@ public class testAPIController {
     @GetMapping("/test")
     private String test(){
         return "Success";
+    }
+    @GetMapping("/home")
+    private String home(@RequestParam(name="nome")String nome){
+        return"Olá, "+nome+". Seja bem-vindo";
     }
 }
