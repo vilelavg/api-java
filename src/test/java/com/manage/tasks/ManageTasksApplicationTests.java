56e1c12c031c08056ad2a24e7a67ef89c3c1d8a9
Vitor Vilela <vitorvilella123@gmail.com>
2024-02-15 15:09:41 -0300

    Primeiro commit

---
+++ src/test/java/com/manage/tasks/ManageTasksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manage.tasks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageTasksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
